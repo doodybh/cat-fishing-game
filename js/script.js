a7018d8d01800c32e4f78badae9dbdcccb4adff0
doodybh <doodybhr38@gmail.com>
2025-12-08 20:48:21 +0300

    tiny update just different picture use.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const mainCatPictureElement = document.getElementById('main-cat-picture');
 
-mainCatPictureElement.addEventListener('mouseover', () => mainCatPictureElement.src = "./images/catstanding.png");
+mainCatPictureElement.addEventListener('mouseover', () => mainCatPictureElement.src = "./images/catmeow.png");
 
 mainCatPictureElement.addEventListener('mouseout', () => mainCatPictureElement.src = "./images/catwalking.png");

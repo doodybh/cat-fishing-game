b395ecdfdd32673523b41d1939dad3c904671b08
doodybh <doodybhr38@gmail.com>
2025-12-13 20:21:19 +0300

    some updates to the modal still WIP

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 const mainCatPictureElement = document.getElementById('main-cat-picture');
+const infoButtonElement = document.getElementById('info-button')
+const infoModalElement = document.getElementById('info-modal')
+const closeButtonElement = document.getElementById('close-button')
+const overlayElement = document.getElementById('overlay')
 
 mainCatPictureElement.addEventListener('mouseover', () => mainCatPictureElement.src = "./images/catmeow.png");
 
 mainCatPictureElement.addEventListener('mouseout', () => mainCatPictureElement.src = "./images/catwalking.png");
+
+
+infoButtonElement.addEventListener('click', () => {
+    infoModalElement.style.display = 'block';
+    overlayElement.classList.add('active');
+});
+
+closeButtonElement.addEventListener('click', () => {
+    infoModalElement.style.display = 'none';
+    overlayElement.classList.remove('active');
+});

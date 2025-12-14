5664d3c8f4fce1883e0db61df969abb25424cfdf
doodybh <doodybhr38@gmail.com>
2025-12-14 19:28:36 +0300

    made the line a bit functional

--- js/game.js
+++ js/game.js
@@ -1,7 +1,10 @@
-const fishingLineElement = document.querySelector('.fishing-line');
+const fishingLineElement = document.getElementById('fishing-line');
 const topOfLine = window.innerHeight * 0.31;
+
 document.addEventListener('mousemove', (mouseEvent) => {
     console.log(mouseEvent.clientY);
     let lineHeight = mouseEvent.clientY - topOfLine;
-fishingLineElement.style.height = lineHeight
+    if (lineHeight < 0) lineHeight = 0;
+    if (lineHeight > window.innerHeight) lineHeight = window.innerHeight; 
+fishingLineElement.style.height = lineHeight + 'px';
 })
